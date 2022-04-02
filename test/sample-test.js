485312eb56f74261ed457835c9034a30b7ce50e3
todesstille <tanatonaut@gmail.com>
2022-04-02 21:51:03 +0300

    chainlink + bash scripts

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,52 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Mock Link Set", function() {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  let MockLinkToken, mockLinkToken, owner, acc1;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  beforeEach(async () => {
+    [owner, acc1] = await ethers.getSigners();
+    
+    MockLinkToken = await ethers.getContractFactory("MockLinkToken");
+    mockLinkToken = await MockLinkToken.deploy();
+    await mockLinkToken.deployed();
+  });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  describe("Mock Link Token", function() {
+    it("Should return correct name and symbol", async () => {  
+      expect(await mockLinkToken.name()).to.equal("ChainLink Token");    
+      expect(await mockLinkToken.symbol()).to.equal("LINK");
+      expect(await mockLinkToken.balanceOf(owner.address)).to.equal("1000000000000000000000000000");
+    });
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    it ("Should transfer 1000 tokens", async () => {
+      const TX = await mockLinkToken.transfer(acc1.address, 1000);
+      await TX.wait();
+      expect(await mockLinkToken.balanceOf(acc1.address)).to.equal("1000");
+      expect(await mockLinkToken.balanceOf(owner.address)).to.equal("999999999999999999999999000");
+    });
   });
 });
+
+/*describe("Mock Link Token", function () {
+
+  beforeEach(async () => {
+    const MockLinkToken = await ethers.getContractFactory("MockLinkToken");
+    const mockLinkToken = await MockLinkToken.deploy();
+    await mockLinkToken.deployed();
+  });
+
+  it("Should return correct name and symbol", async function () {
+
+    expect(await mockLinkToken.name()).to.equal("ChainLink Token");    
+    expect(await mockLinkToken.symbol()).to.equal("LINK");
+
+    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // wait until the transaction is mined
+    // await setGreetingTx.wait();
+
+    // expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});*/
